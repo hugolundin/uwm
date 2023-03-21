9949a646dd2f719c928c466845bd239002e7bb0c
Hugo Lundin <hugo@lundin.dev>
2023-03-21 14:37:50 +0100

    Changes

--- src/server.cpp
+++ src/server.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
-#include "rpc/server.h"
+
+#include "nngpp/nngpp.h"
+#include "nngpp/protocol/sub0.h"
+#include "nngpp/platform/platform.h"
+
 #include "readerwriterqueue.h"
 
 int main(int argc, char const *argv[])
 {
-    moodycamel::ReaderWriterQueue<int> q;
+    // moodycamel::ReaderWriterQueue<int> q;
     
-    int number;
-    q.enqueue(5);
-    auto res = q.try_dequeue(number);
+    // int number;
+    // q.enqueue(5);
+    // auto res = q.try_dequeue(number);
 
-    std::cout << number << std::endl;
+    // std::cout << number << std::endl;
+
+    auto s = nng::sub::open();
+    s.set_opt_string(NNG_OPT_SUB_SUBSCRIBE, {});
+    s.dial("ipc://@pubsub.ipc");
+
+    for (;;) {
+        auto message = s.recv(NNG_FLAG_ALLOC);
+        nng::msleep(1000);
+    }
 
     // rpc::server server(rpc::constants::DEFAULT_PORT);
 
